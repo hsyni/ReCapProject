b3ced458897a28d5a76219b8e61e46bcfbf9ae7e
huseyni <huseyinozgurkazan@gmail.com>
2022-07-18 12:13:03 +0300

    18.07.2022 12:12

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,7 +3,7 @@ using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 
-CarManager carManager = new CarManager(new EfCarDal());
+//CarManager carManager = new CarManager(new EfCarDal());
 
 //foreach (var car in carManager.GetAll())
 //{
@@ -24,9 +24,17 @@ CarManager carManager = new CarManager(new EfCarDal());
 //    Console.WriteLine(brand.Name);
 //}
 
-var result = carManager.GetCarDetail();
+//var result = carManager.GetCarDetail();
 
-foreach (var car in result.Data)
-{
-    Console.WriteLine(car.CarName + "/" + car.ColorName + "/" + car.DailyPrice);
-}
+//foreach (var car in result.Data)
+//{
+//    Console.WriteLine(car.CarName + "/" + car.ColorName + "/" + car.DailyPrice);
+//}
+
+//CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+//customerManager.Insert(new Customer() { UserId = 1, CompanyName = "Danistay", Id = 1 });
+
+RentalManager rentalManager = new RentalManager(new EfRentalDal());
+
+rentalManager.Insert(new Rental() { CarId = 1, CustomerId = 1, Id = 1, RentDate = new DateTime(2022, 07, 17), ReturnDate = new DateTime(2022, 07, 18) });
